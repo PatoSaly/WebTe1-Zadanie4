c2856c0121a949db31e5a0c0c214fb645a809750
PatoSaly <saly.pato@gmail.com>
2020-10-28 17:51:49 +0100

    Zadanie je hotove

--- js/javascript.js
+++ js/javascript.js
@@ -1,17 +1,70 @@
-function toggleModal (hidden) {
-    document.getElementById('fade').style.display = hidden ? "none" : "block";
-    document.getElementByClass('modal-container')[0].style.display = hidden ? "none" : "flex";
+var modalWindow = document.getElementById('modal-container');
+
+function showModal() {
+    x = document.getElementById("numberX");
+    y = document.getElementById("numberY");
+    if(!numberValidate(x.value, x.id) || !numberValidate(y.value, y.id)) {
+        numberValidate(y.value, y.id);
+        return;
+    }
+
+    modalWindow.style.display = "block";
+    generateTable();
+}
+function closeModal() {
+    modalWindow.style.display = "none";
+}
+window.onclick = function(event) {
+    if(event.target == modalWindow) {
+        modalWindow.style.display = "none";
+    }
+}
+
+function foo(x, y) {
+    return x*y;
 }
 
 function generateTable () {
-    var table = document.createElement("table");
+    if(document.getElementsByTagName("table").length == 1){
+        document.getElementsByTagName("table")[0].remove();
+    }
 
-    for(let i = 0; i < 5; i++){
+    var x = document.getElementById("numberX").value;
+    var y = document.getElementById("numberY").value;
+
+
+    var table = document.createElement("table");
+    table.classList.add("table");
+
+    for(let i = 0; i <= x; i++){
         let row = document.createElement("tr");
-        for(let j = 0; j < 10; j++){
-            let col = document.createElement("td");
-            row.appendChild(col);
-            col.append((j+1) + ".");
+    
+        if(i == 0){
+            //Vytvorenie prveho riadka
+            let cellH = document.createElement("th");
+            let cellText = document.createTextNode("#");
+            cellH.appendChild(cellText);
+            row.appendChild(cellH);
+            for(let j = 1; j <= y; j++){
+                let cellH = document.createElement("th");
+                let cellText = document.createTextNode("Y = " + j);
+                cellH.appendChild(cellText);
+                row.appendChild(cellH);
+            }
+            table.appendChild(row);
+            continue;
+        }
+        //Vytvorenie zvysku tabluky, prve je th s hodnotou x
+        let cellH = document.createElement("th");
+        let cellText = document.createTextNode("X = " + i);
+        cellH.appendChild(cellText);
+        row.appendChild(cellH)
+
+        for(let j = 1; j <= y; j++){
+            let cell = document.createElement("td");
+            let cellText = document.createTextNode(foo(i,j));
+            cell.appendChild(cellText);
+            row.appendChild(cell);
         }
         table.appendChild(row);
     }
@@ -19,12 +72,18 @@ function generateTable () {
     document.getElementById("table-container").appendChild(table);
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("table-button").addEventListener("click", generateTable);
 
-    document.getElementById("event-input").addEventListener("keyup", function(event) {
-        console.log(event.target.value);
-    })
-});
+function numberValidate (n, id) {
+    let element = document.getElementById(id).nextSibling.nextSibling;
+
+    if(n < 1 || n > 9 || n == "") {
+        element.className -= " closed";
+        return 0;
+    }
+    else {
+        element.className += " closed";
+        return 1;
+    }
+}
 
 
